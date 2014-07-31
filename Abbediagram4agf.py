edb9cc7a8afc0516efabdf50be83f230bc73f127
imyu37 <wanyong_37@hotmail.com>
2014-07-31 14:11:35 +0800

    Update Abbediagram4agf.py

--- Abbediagram4agf.py
+++ Abbediagram4agf.py
@@ -1,38 +1,57 @@
 #coding:utf-8
 '''
-read glass name,Abbe number and refractive index in a .agf file,and plot the Abbe diagram
+Read a .AGF file,and plot the Abbe diagram
 '''
 
 from matplotlib.widgets import Cursor
 import matplotlib.pyplot as plt
 
-file = open('schott.agf','r')
-#file = open('cdgm.agf','r')
-gname = list() #glass name
-vd = list() #Abbe number
-nd = list() #index of d line
+fig = plt.figure(figsize=(8, 6))
+ax = plt.axes()
+
+#file = open('schott.agf','r')
+file = open('cdgm.agf','r')
+points_with_annotation = list() # data point with annotation
 lines = file.readlines()
 for line in lines:
 	tmp = line.split()
+#	vtmp = float(tmp[4])
+#	ntmp = float(tmp[5])
 	if tmp[0] == 'NM':
-		gname.append(tmp[1])
-		vd.append(float(tmp[4]))
-		nd.append(float(tmp[5]))
+		point, = plt.plot(float(tmp[5]), float(tmp[4]), 'o', markersize=10)
+
+		annotation = ax.annotate("%s $nd$=%f $vd$=%f" % (tmp[1],float(tmp[4]), float(tmp[5])),
+			xy=(float(tmp[5]), float(tmp[4])), xycoords='data',
+			xytext=(float(tmp[5])-2.5, float(tmp[4])), textcoords='data',
+			horizontalalignment="left",
+			bbox=dict(boxstyle="round", facecolor="w",
+			edgecolor="0.5", alpha=0.9)
+			)
+# by default, disable the annotation visibility
+		annotation.set_visible(False)
+		points_with_annotation.append([point, annotation])
 
 file.close()
 
-fig = plt.figure(figsize=(8, 6))
-ax = fig.add_subplot(111, axisbg='#FFFFFF')
-ax.plot(nd,vd,'o',color='#33FF00')
-ax.set_xlim(90,5)
+ax.set_xlim(100,5)
 ax.set_ylim(1.4, 2.05)
 ax.set_xlabel('Abbe number $V$ ')
 ax.set_ylabel('Refractive Index $n_d$ ($\lambda_d$=587.6nm) ')
 
-# Turn on for lableing every data point with glassname
-for i, txt in enumerate(gname):
-	ax.annotate(txt, (nd[i],vd[i]))
+# Thanks for  pelson(https://stackoverflow.com/users/741316/pelson) provide the  mathod in stackoverflow.com
+# https://stackoverflow.com/questions/11537374/matplotlib-basemap-popup-box#new-answer
+def on_move(event):
+    visibility_changed = False
+    for point, annotation in points_with_annotation:
+        should_be_visible = (point.contains(event)[0] == True)
 
-cursor = Cursor(ax, useblit=True, color='red', linewidth=2 )
+        if should_be_visible != annotation.get_visible():
+            visibility_changed = True
+            annotation.set_visible(should_be_visible)
+
+    if visibility_changed:
+        plt.draw()
+
+on_move_id = fig.canvas.mpl_connect('motion_notify_event', on_move)
 
 plt.show()
